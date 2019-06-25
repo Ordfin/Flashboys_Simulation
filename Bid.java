0dfc8b7e5db09f1dc3feea60d921752d79bae92d
Tyastrab <38965052+Tyastrab@users.noreply.github.com>
2019-06-25 13:19:27 -0400

    Update Bid.java

--- Bid.java
+++ Bid.java
@@ -11,7 +11,6 @@ public class Bid {
 		time = t;
 		amount = amt;
 		player = p; 
-		System.out.println("Player " + p + " bid $" + amt + " at time " + t);
 	}
 	
 	public static void trying(int i) {
