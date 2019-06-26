b95fe95b552355f6bebd8246989f3f54097abe05
Tova Blau <tovablau01@gmail.com>
2019-06-26 16:14:37 -0400

    fixed bid sequence for bids placed at same time

--- ReactiveCounterbidding.java
+++ ReactiveCounterbidding.java
@@ -2,11 +2,18 @@ import java.util. *;
 
 public class ReactiveCounterbidding extends Strategy{
 	
-	public void run(double t, double D, double s, int i, double iota, double epsilon, double loss_func, ArrayList<Bid> allBids, ArrayList<Bid> bidsPlayer, int profit) {	
+	public void run(double t, double D, double s, int i, double iota, double epsilon, double loss_func, ArrayList<Bid> allBids, ArrayList<Bid> bidsPlayer, double profit, TreeMap<Double, ArrayList<Bid>> temp) {	
 		
 		if (allBids.isEmpty()) {
-			Bid b = new Bid(t, s, i);  
-			allBids.add(b);
+			Bid b = new Bid(t, s, i);
+			if(temp.containsKey(s)) {
+				temp.get(s).add(b);	
+			}
+			else if (!temp.containsKey(s)) {
+				ArrayList<Bid> tempList = new ArrayList<>();
+				tempList.add(b);
+				temp.put(s, tempList);
+			}				
 			bidsPlayer.add(b);
 		}
 		else if(bidsPlayer.isEmpty()) {
@@ -14,7 +21,14 @@ public class ReactiveCounterbidding extends Strategy{
 					iota), allBids.get(allBids.size()-1).getAmount() + epsilon),
 					profit);
 			Bid b = new Bid(t, a, i);
-			allBids.add(b);
+			if(temp.containsKey(a)) {
+				temp.get(a).add(b);	
+			}
+			else if (!temp.containsKey(a)) {
+				ArrayList<Bid> tempList = new ArrayList<>();
+				tempList.add(b);
+				temp.put(a, tempList);
+			}				
 			bidsPlayer.add(b);
 		}
 		else {
@@ -24,7 +38,14 @@ public class ReactiveCounterbidding extends Strategy{
 						iota), allBids.get(allBids.size()-1).getAmount() + epsilon),
 						profit + loss_func * bidsPlayer.get(bidsPlayer.size()-1).getAmount());
 				Bid b = new Bid(t, a, i);
-				allBids.add(b);
+				if(temp.containsKey(a)) {
+					temp.get(a).add(b);	
+				}
+				else if (!temp.containsKey(a)) {
+					ArrayList<Bid> tempList = new ArrayList<>();
+					tempList.add(b);
+					temp.put(a, tempList);
+				}				
 				bidsPlayer.add(b);
 			}
 		}
