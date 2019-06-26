3efee10cb659f80b5894d004ed8f082007ea742a
Tamar Yastrab <tyastrab@gmail.com>
2019-06-26 11:24:10 -0400

    special cases accounted for

--- BlindRaising.java
+++ BlindRaising.java
@@ -10,13 +10,16 @@ public class BlindRaising extends Strategy{
 	private static int k;
 	
 	
-	public void run(double t, double D, double s, int i, double iota, double epsilon, double loss_func, ArrayList<Bid> allBids, ArrayList<Bid> bidsPlayer) {	
+	public void run(double t, double D, double s, int i, double iota, double epsilon, double loss_func, ArrayList<Bid> allBids, ArrayList<Bid> bidsPlayer, double profit) {	
 		if(t<D) {
 			// bids (starting_amount * (1+f)^k) every "wait" seconds
 			if (t % wait == 0) {
 				double amt = Math.pow(s *(1+f), k);
-				if (amt >= 1 + loss_func * bidsPlayer.get(bidsPlayer.size()-1).getAmount()) return;
-				if (!(bidsPlayer.isEmpty())) {
+				if (bidsPlayer.isEmpty()) {
+					if (amt >= profit) return;
+				}
+				else if (amt >= profit + loss_func * bidsPlayer.get(bidsPlayer.size()-1).getAmount()) return;
+				else if (!(bidsPlayer.isEmpty())) {
 					if (amt < allBids.get(allBids.size()-1).getAmount()) 
 						return; 
 				}
--- ReactiveCounterbidding.java
+++ ReactiveCounterbidding.java
@@ -2,7 +2,7 @@ import java.util. *;
 
 public class ReactiveCounterbidding extends Strategy{
 	
-	public void run(double t, double D, double s, int i, double iota, double epsilon, double loss_func, ArrayList<Bid> allBids, ArrayList<Bid> bidsPlayer) {	
+	public void run(double t, double D, double s, int i, double iota, double epsilon, double loss_func, ArrayList<Bid> allBids, ArrayList<Bid> bidsPlayer, double profit) {	
 		
 		if (allBids.isEmpty()) {
 			Bid b = new Bid(t, s, i);  
@@ -10,7 +10,9 @@ public class ReactiveCounterbidding extends Strategy{
 			bidsPlayer.add(b);
 		}
 		else if(bidsPlayer.isEmpty()) {
-			double a = s + 1;
+			double a = Math.min(Math.max(allBids.get(allBids.size()-1).getAmount() * (1+
+					iota), allBids.get(allBids.size()-1).getAmount() + epsilon),
+					profit);
 			Bid b = new Bid(t, a, i);
 			allBids.add(b);
 			bidsPlayer.add(b);
@@ -18,9 +20,9 @@ public class ReactiveCounterbidding extends Strategy{
 		else {
 			if ((allBids.get(allBids.size()-1).getPlayer() != i) 
 					&& (allBids.get(allBids.size()-1).getAmount() > bidsPlayer.get(bidsPlayer.size()-1).getAmount())) {
-				double a = Math.min(Math.max(bidsPlayer.get(bidsPlayer.size()-1).getAmount() * (1+
-						iota), bidsPlayer.get(bidsPlayer.size()-1).getAmount() + epsilon),
-						1 + loss_func * bidsPlayer.get(bidsPlayer.size()-1).getAmount());
+				double a = Math.min(Math.max(allBids.get(allBids.size()-1).getAmount() * (1+
+						iota), allBids.get(allBids.size()-1).getAmount() + epsilon),
+						profit + loss_func * bidsPlayer.get(bidsPlayer.size()-1).getAmount());
 				Bid b = new Bid(t, a, i);
 				allBids.add(b);
 				bidsPlayer.add(b);
