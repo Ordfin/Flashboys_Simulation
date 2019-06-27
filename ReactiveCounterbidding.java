721ad0a267b8d247fdc80a044622c5a229263b93
Tamar Yastrab <tyastrab@gmail.com>
2019-06-27 10:11:37 -0400

    latency included as a var

--- ReactiveCounterbidding.java
+++ ReactiveCounterbidding.java
@@ -1,15 +1,19 @@
 import java.util. *;
 
+import javax.swing.JOptionPane;
+
 public class ReactiveCounterbidding extends Strategy{
+	
+	private double latency;
 	
 	public void run(double t, double D, double s, int i, double iota, double epsilon, double loss_func, ArrayList<Bid> allBids, ArrayList<Bid> bidsPlayer, double profit) {	
 		
-		if (allBids.isEmpty()) {
+		if (allBids.isEmpty()) { //if no bid yet, place first bid
 			Bid b = new Bid(t, s, i);  
 			allBids.add(b);
 			bidsPlayer.add(b);
 		}
-		else if(bidsPlayer.isEmpty()) {
+		else if(bidsPlayer.isEmpty()) { //if you haven't bid yet
 			double a = Math.min(Math.max(allBids.get(allBids.size()-1).getAmount() * (1+
 					iota), allBids.get(allBids.size()-1).getAmount() + epsilon),
 					profit);
@@ -17,7 +21,7 @@ public class ReactiveCounterbidding extends Strategy{
 			allBids.add(b);
 			bidsPlayer.add(b);
 		}
-		else {
+		else { //if both players have bid
 			if ((allBids.get(allBids.size()-1).getPlayer() != i) 
 					&& (allBids.get(allBids.size()-1).getAmount() > bidsPlayer.get(bidsPlayer.size()-1).getAmount())) {
 				double a = Math.min(Math.max(allBids.get(allBids.size()-1).getAmount() * (1+
@@ -28,6 +32,11 @@ public class ReactiveCounterbidding extends Strategy{
 				bidsPlayer.add(b);
 			}
 		}
+	}
+	
+	public void enterValues() {
+			
+	    	this.latency = Double.parseDouble(JOptionPane.showInputDialog("Enter fractional increment"));
 	}
 
 }
